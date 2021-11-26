a5846db79cf68b870f6437d2f6a0c815239aa05f
David Deaton <dideater@gmail.com>
2021-11-26 13:43:52 -0800

    homepage completed, mostly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function endorsementMovement () {
 
     console.log(endorNumOld);
 
-    let endorNum = Math.floor(Math.random()*6+1);
+    let endorNum = Math.floor(Math.random()*7+1);
 
     console.log("endorsement number", endorNum);
 
@@ -13,65 +13,33 @@ function endorsementMovement () {
     }
 
     if (endorNum === 0) {
-        endorNum = 6;
+        endorNum = 7;
     }
 
     console.log("endorsement number", endorNum);
 
     localStorage.setItem("Endo 1", endorNum);
 
-    $("#endor1").removeClass("grid1");
-    $("#endor2").removeClass("grid1");
-    $("#endor3").removeClass("grid1");
-    $("#endor4").removeClass("grid1");
-    $("#endor5").removeClass("grid1");
-    $("#endor6").removeClass("grid1");
+    $("#endor1").addClass("none");
+    $("#endor2").addClass("none");
+    $("#endor3").addClass("none");
+    $("#endor4").addClass("none");
+    $("#endor5").addClass("none");
+    $("#endor6").addClass("none");
+    $("#endor7").addClass("none");
 
-    $("#endor" + endorNum).addClass("grid1");
+    $("#endor1").removeClass("reviewDiv");
+    $("#endor2").removeClass("reviewDiv");
+    $("#endor3").removeClass("reviewDiv");
+    $("#endor4").removeClass("reviewDiv");
+    $("#endor5").removeClass("reviewDiv");
+    $("#endor6").removeClass("reviewDiv");
+    $("#endor7").removeClass("reviewDiv");
+
+    $("#endor" + endorNum).removeClass("none");
+    $("#endor" + endorNum).addClass("reviewDiv");
 
 };
 
 endorsementMovement();
 setInterval(endorsementMovement, 30000);
-
-
-
-// const timeHM = moment().format('ss');
-// const timeIntHM = parseInt(timeHM);
-// let refresh = 60;
-
-// if (timeIntHM < 30) {
-//     refresh = 30 - timeIntHM;
-// }
-
-// else {
-//     refresh = 60 - timeIntHM;
-// }
-
-// console.log (refresh);
-
-
-function chooseEndorsement () {
-
-    console.log("is this working");
-
-    const endorNumOld = localStorage.getItem("Endo 1");
-
-    let endorNum = Math.floor(Math.random()*6+1);
-
-    console.log("endorsement number", endorNum);
-
-    if (endorNum === endorNumOld && endorNumOld === 6) {
-        endorNum--
-    }
-
-    else if (endorNum === endorNumOld) {
-        endorNum++
-    }
-
-    console.log("endorsement number", endorNum);
-
-    localStorage.setItem("Endo 1", endorNum);
-}
-
-chooseEndorsement ();
